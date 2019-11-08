f6aba65b703e2fba4a849a5570071c3c721cf07d
dennis <dennisdxf@gmail.com>
2019-11-08 23:06:03 +0800

    rm last n node & find mid node

---
+++ src/main/java/org/zhare/leetcode/algorithms/list/FindMiddleNode.java
@@ -0,0 +1,60 @@
+package org.zhare.leetcode.algorithms.list;
+
+/**
+ * @author dennis
+ * @since 2019/11/8
+ */
+public class FindMiddleNode {
+    private static Node head;
+
+    public static void main(String[] args) {
+        int[] values = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        head = build(values);
+        print(head);
+        Node mid = findMiddleNode(head);
+        System.out.println(mid == null ? "null" : mid.val);
+    }
+
+    private static Node findMiddleNode(Node head) {
+        Node slow = head;
+        Node fast = slow;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    private static void print(Node head) {
+        Node node = head;
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    private static Node build(int[] values) {
+        if (values == null || values.length < 1) return null;
+        Node head = new Node(values[0]);
+        Node cur = head;
+        for (int i = 1; i < values.length; ++i) {
+            Node node = new Node(values[i]);
+            cur.next = node;
+            cur = node;
+        }
+
+        return head;
+    }
+
+
+    static class Node {
+        private int val;
+        private Node next;
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+}
---
+++ src/main/java/org/zhare/leetcode/algorithms/list/RemoveLastNNode.java
@@ -0,0 +1,80 @@
+package org.zhare.leetcode.algorithms.list;
+
+/**
+ * @author dennis
+ * @since 2019/11/8
+ */
+public class RemoveLastNNode {
+    /*
+     * 删除倒数第n个节点
+     *
+     * 双指针
+     *
+     * */
+
+    private static Node head;
+
+    public static void main(String[] args) {
+        int[] values = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        head = build(values);
+        print(head);
+        head = removeLastN(head, 5);
+        print(head);
+    }
+
+    private static Node removeLastN(Node head, int n) {
+        Node slow = head;
+        Node fast = slow;
+        while (fast != null && n > 0) {
+            n--;
+            fast = fast.next;   // index is (n+1)th node
+        }
+
+        if (fast == null) {
+            head = head.next;
+            return head;
+        }
+
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return head;
+
+    }
+
+    private static void print(Node head) {
+        Node cur = head;
+        while (cur != null) {
+            System.out.print(cur.val + " ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
+
+    private static Node build(int[] values) {
+        if (values == null || values.length < 1) return null;
+        Node head = new Node(values[0]);
+        Node cur = head;
+        for (int i = 1; i < values.length; ++i) {
+            Node node = new Node(values[i]);
+            cur.next = node;
+            cur = node;
+        }
+
+        return head;
+    }
+
+
+    static class Node {
+        private int val;
+        private Node next;
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+}

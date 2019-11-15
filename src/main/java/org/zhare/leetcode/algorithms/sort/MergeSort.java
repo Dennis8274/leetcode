098c350074b3046963bc7c1da1226e36b9c5897a
dennis <dennisdxf@gmail.com>
2019-11-15 23:26:47 +0800

    merge sort

---
+++ src/main/java/org/zhare/leetcode/algorithms/sort/MergeSort.java
@@ -0,0 +1,65 @@
+package org.zhare.leetcode.algorithms.sort;
+
+import java.util.Arrays;
+
+/**
+ * @author dennis
+ * @since 2019/11/15
+ */
+public class MergeSort {
+    /*
+    *
+    * 归并排序
+    * 稳定
+    *
+    * 时间复杂度：O(nlogn) 最好 最坏 平均
+    * 空间复杂度：O(n)
+    *
+    * */
+
+    public static void main(String[] args) {
+        int[] values = new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1};
+        sort(values, 0, values.length - 1);
+        System.out.println(Arrays.toString(values));
+    }
+
+    private static void sort(int[] values, int l, int r) {
+        if (l >= r) return;
+        int m = (l + r) / 2;
+        sort(values, l, m);
+        sort(values, m + 1, r);
+        merge(values, l, m, r);
+
+    }
+
+    private static void merge(int[] values, int l, int m, int r) {
+        int[] temp = new int[r - l + 1];
+        int i = l;
+        int j = m + 1;
+        int k = 0;
+        while (i <= m && j <= r) {
+            if (values[i] <= values[j]) {
+                temp[k++] = values[i++];
+            } else {
+                temp[k++] = values[j++];
+            }
+        }
+
+        int start = i;
+        int end = m;
+        if (j <= r) {
+            start = j;
+            end = r;
+        }
+
+        while (start <= end) {
+            temp[k++] = values[start++];
+        }
+
+        for (int n = 0; n < r - l + 1; ++n) {
+            values[l + n] = temp[n];
+        }
+
+
+    }
+}

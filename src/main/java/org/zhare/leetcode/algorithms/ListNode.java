20ef1e7a5bb6bcc93ca46aeddbed39c3888ffc8b
dennis <dennisdxf@gmail.com>
2019-12-28 19:05:01 +0800

    common ops method moved to ListNode

--- src/main/java/org/zhare/leetcode/algorithms/ListNode.java
+++ src/main/java/org/zhare/leetcode/algorithms/ListNode.java
@@ -11,4 +11,27 @@ class ListNode {
     ListNode(int x) {
         val = x;
     }
+
+    static ListNode build(int[] nums) {
+        if (nums == null || nums.length < 1) return null;
+        ListNode root = new ListNode(nums[0]);
+        ListNode node = root;
+        for (int i = 1; i < nums.length; ++i) {
+            ListNode next = new ListNode(nums[i]);
+            node.next = next;
+            node = next;
+        }
+
+        return root;
+    }
+
+    static void print(ListNode root) {
+        ListNode cur = root;
+        while (cur != null) {
+            System.out.print(cur.val + " ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
+
 }
--- src/main/java/org/zhare/leetcode/algorithms/TwoZeroSix.java
+++ src/main/java/org/zhare/leetcode/algorithms/TwoZeroSix.java
@@ -1,5 +1,8 @@
 package org.zhare.leetcode.algorithms;
 
+import static org.zhare.leetcode.algorithms.ListNode.build;
+import static org.zhare.leetcode.algorithms.ListNode.print;
+
 /**
  * @author dennis
  * @since 2019/12/28
@@ -47,28 +50,6 @@ public class TwoZeroSix {
         node.next = null;
 
         return root;
-    }
-
-    private static ListNode build(int[] nums) {
-        if (nums == null || nums.length < 1) return null;
-        ListNode root = new ListNode(nums[0]);
-        ListNode node = root;
-        for (int i = 1; i < nums.length; ++i) {
-            ListNode next = new ListNode(nums[i]);
-            node.next = next;
-            node = next;
-        }
-
-        return root;
-    }
-
-    private static void print(ListNode root) {
-        ListNode cur = root;
-        while (cur != null) {
-            System.out.print(cur.val + " ");
-            cur = cur.next;
-        }
-        System.out.println();
     }
 
 }
